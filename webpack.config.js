53559bc933e360dfedbf534f1d870c32eb8e1c54
vino <vino0908@outlook.com>
2021-05-24 21:54:16 +0800

    添加webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist/lib'),
         library: 'VinoReactWheels',
-        // 别问为啥要加``,因为不加会有警告
-        libraryTarget: `"umd"`,
+        // 别问为啥要用``,因为不加会有警告
+        libraryTarget: `umd`,
     },
     module: {
         rules: [
